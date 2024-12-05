527e947fd4f4af3ae4a0e92ad8f9e59155f95a77
Dilum paranavithana <dilumchandupa@gmail.com>
2024-12-05 15:48:13 +0530

    feat: :art: Store setup
    created a store and slice file and made the store access the it.

---
+++ src/features/postsSlice.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [{
+    id: 1, 
+    title: 'first post',
+    content: 'this is the first post'
+},
+{
+    id: 2,
+    title: 'second post',
+    content: 'this is the second post'
+}
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers:{}
+})
+
+export default postsSlice.reducer;
+
+export const selectAllPosts = (state) => state.posts;
