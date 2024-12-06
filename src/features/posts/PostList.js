fc89ba66cb89b709879e503c9e585dec513ff44a
Dilum paranavithana <dilumchandupa@gmail.com>
2024-12-06 14:58:50 +0530

    feat: :art: Recent post on top
    made the most recent post appear on top.

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -7,7 +7,9 @@ import TimeAgo from './TimeAgo';
 function PostList() {
     const posts = useSelector(selectAllPosts);
 
-    const renderedPosts = posts.map(post => (
+    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+
+    const renderedPosts = orderedPosts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0,100)}</p>
