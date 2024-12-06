a66a9b67a349b3cbcb004cc20f20b12279fde5b0
Dilum paranavithana <dilumchandupa@gmail.com>
2024-12-06 14:53:56 +0530

    feat: :art: Time stamp
    added a time stamp for when the post was created.

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectAllPosts } from './postsSlice'
 import PostAuthor from './postAuthor';
+import TimeAgo from './TimeAgo';
 
 function PostList() {
     const posts = useSelector(selectAllPosts);
@@ -10,7 +11,10 @@ function PostList() {
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0,100)}</p>
-            <p className='postCredit'><PostAuthor userId={post.userId}/></p>
+            <p className='postCredit'>
+              <PostAuthor userId={post.userId}/>
+              <TimeAgo timestamp={post.date}/>
+            </p>
         </article>
     ))
   return (
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,18 @@
 
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [{
     id: 1, 
     title: 'first post',
-    content: 'this is the first post'
+    content: 'this is the first post',
+    date:sub(new Date(), {minutes: 10}).toISOString()
 },
 {
     id: 2,
     title: 'second post',
-    content: 'this is the second post'
+    content: 'this is the second post',
+    date:sub(new Date(), {minutes: 5}).toISOString()
 }
 ]
 
@@ -27,6 +30,7 @@ const postsSlice = createSlice({
                     id: nanoid(),
                     title,
                     content,
+                    date: new Date().toISOString(),
                     userId
                 }
             }
